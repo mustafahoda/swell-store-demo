859d599695183543dd4db447948b36a015c086b7
Mustafa Hoda <mustafahoda@Mustafas-MBP-2.attlocal.net>
2022-08-13 21:49:34 -0700

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Breadcrumb, Layout, Menu, Button } from "antd";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from "react";
+
+import { BrowserRouter, Link, Routes, Route, Router } from "react-router-dom";
+
+import "./App.css";
+const swell = require("swell-js");
+const { Header, Content, Footer } = Layout;
+
+swell.init("mustafa-swell-demo", "pk_KyTi02he8noWb4FeGquWbGzvf4XwSszs");
+
+const menuItems = [
+  {
+    label: "Products",
+    key: "product",
+  },
+  {
+    label: "Cart",
+    key: "cart",
+  },
+];
+
+const App = () => (
+  <BrowserRouter>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        {/* <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={menuItems}
+        /> */}
+        <Menu theme="dark" mode="horizontal">
+          <Link to="/products">
+            <Menu.Item>Products</Menu.Item>
+          </Link>
+          <Link to="cart">
+            <Menu.Item>Cart</Menu.Item>
+          </Link>
+        </Menu>
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+
+        <Routes>
+          <Route path="/products" element={<Products />}></Route>
+        </Routes>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Mustafa Hoda + Swell © 2022 Created w/ Ant + React
+      </Footer>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/Products.tsx
@@ -0,0 +1,173 @@
+import * as React from "react";
+import { Space, Table, Button, Statistic } from "antd";
+import Column from "antd/lib/table/Column";
+const swell = require("swell-js");
+
+swell.init(
+  process.env.REACT_APP_SWELL_STORE_ID,
+  process.env.REACT_APP_SWELL_PUBLIC_KEY
+);
+
+interface ProductsProps {}
+
+interface ProductsState {
+  products: any;
+  cartValue: number;
+  // TODO: Fix the typing
+  cart: {
+    [key: string]: {
+      quantity: number;
+    };
+  };
+}
+
+class Products extends React.Component<ProductsProps, ProductsState> {
+  async getProducts() {
+    try {
+      let products = await swell.products.list({
+        limit: 25, // Max. 100
+        page: 1,
+      });
+
+      return products;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  addToCart(props: { id: string }) {
+    let itemToAdd: string = props.id;
+
+    // If the item already exists, update the quantity
+    if (this.state.cart.hasOwnProperty(itemToAdd)) {
+      console.log("The item already is in the cart. Updating quantity");
+      this.state.cart[itemToAdd]["quantity"]++;
+    }
+    // If the item isn't in the cart, then add it to the cart
+    else {
+      console.log("The item isn't in the cart. Adding to cart");
+      this.state.cart[itemToAdd] = { quantity: 1 };
+    }
+
+    // Update the cart value
+    // Find the value of the item that is being added
+    let foundProduct = this.state.products.results.find(
+      (x: any) => x.id === itemToAdd
+    );
+
+    this.setState({
+      cartValue: this.state.cartValue + foundProduct.price,
+    });
+
+    // this.state.cartValue = this.state.cartValue + foundProduct.price;
+
+    console.log(this.state);
+  }
+
+  getCartValue() {}
+
+  COLUMNS = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      name: "name",
+    },
+    {
+      title: "Photo",
+      dataIndex: "photo",
+      name: "photo",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      name: "description",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      name: "price",
+    },
+    {
+      title: "Add to Cart",
+      dataIndex: "addToCart",
+      name: "addToCart",
+    },
+  ];
+
+  constructor(props: ProductsProps) {
+    super(props);
+    this.state = {
+      cartValue: 0,
+      products: {},
+      cart: {},
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      cartValue: 0,
+      products: await this.getProducts(),
+      cart: {},
+    });
+
+    console.log(this.state.cart);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Products</h1>
+        {/* <Table
+          dataSource={this.state.products.results}
+          columns={this.COLUMNS}
+        ></Table> */}
+        <Statistic
+          title="Total Cart Value"
+          value={this.state.cartValue}
+          precision={2}
+        ></Statistic>
+        <Table dataSource={this.state.products.results}>
+          <Column title="Name" dataIndex="name" key="name"></Column>
+          <Column
+            title="Description"
+            dataIndex="description"
+            key="description"
+          ></Column>
+          <Column
+            title="Photo"
+            dataIndex={["images", 0, "file", "url"]}
+            key="photo"
+            render={(_: any, record: any) => {
+              return <img src={_} width="100"></img>;
+            }}
+          ></Column>
+          <Column
+            title="Price"
+            dataIndex="price"
+            key="price"
+            defaultSortOrder="descend"
+            sorter={(a: any, b: any) => a.price - b.price}
+          ></Column>
+          <Column
+            title="Action"
+            key="action"
+            render={(_: any, record: any) => {
+              return (
+                <Space size="middle">
+                  <Button
+                    type="primary"
+                    onClick={() => this.addToCart({ id: _.id })}
+                  >
+                    Add to Cart
+                  </Button>
+                </Space>
+              );
+            }}
+          ></Column>
+        </Table>
+      </div>
+    );
+  }
+}
+
+export default Products;
